d077611ff02dbc5a9f038b13dd8b021813a9553e
www-websites <117638652+www-websites@users.noreply.github.com>
2023-03-14 15:54:54 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,34 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    const script_button = document.querySelector(".delete__button");
+    console.log("ewgep]oinputValue");
+    script_button.addEventListener("click", (function(e) {
+        const input = document.querySelector(".who-delete__input");
+        const inputValue = input.value;
+        alert(`З своєї посади було звільнено ${inputValue}`);
+    }));
+    window["FLS"] = true;
+    isWebp();
+})();
